4e756538ae72d2eb9216a64a3d91492118e2c524
hyobiin9 <moone1234@kakao.com>
2025-03-12 10:57:29 +0900

    약수의 개수와 덧셈 / 1단계
    https://school.programmers.co.kr/learn/courses/30/lessons/77884

--- hyobiin9/20250312/01.js
+++ hyobiin9/20250312/01.js
@@ -1,3 +1,11 @@
+// 문제 설명
+// 두 정수 left와 right가 매개변수로 주어집니다.
+// left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고,
+// 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
+
+// 완전 제곱수인 경우에만 약수의 개수가 홀수라는 점을 이용.
+// Math.sqrt(x) 함수를 사용해서 제곱수를 계산하고, Number.isInteger를 사용하여 소수점이 있는지 판단함.
+
 function solution(left, right) {
   var answer = 0;
 
