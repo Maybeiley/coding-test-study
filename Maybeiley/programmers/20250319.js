a824c6d2ab20b8545f7142111231b537b9514d3b
Maybeiley <2784519@gmail.com>
2025-03-19 14:38:11 +0900

    카펫 / 2단계
    https://school.programmers.co.kr/learn/courses/30/lessons/42842

---
+++ Maybeiley/programmers/20250319.js
@@ -0,0 +1,25 @@
+/*
+[카펫]
+Leo는 카펫을 사러 갔다가 아래 그림과 같이 중앙에는 노란색으로 칠해져 있고 테두리 1줄은 갈색으로 칠해져 있는 격자 모양 카펫을 봤습니다.
+Leo는 집으로 돌아와서 아까 본 카펫의 노란색과 갈색으로 색칠된 격자의 개수는 기억했지만, 전체 카펫의 크기는 기억하지 못했습니다.
+Leo가 본 카펫에서 갈색 격자의 수 brown, 노란색 격자의 수 yellow가 매개변수로 주어질 때 카펫의 가로, 세로 크기를 순서대로 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+- 제한사항 : 카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
+*/
+
+function solution(brown, yellow) {
+  var answer = [];
+
+  for (let w = 1; w <= yellow; w++) {
+    let h = yellow / w;
+    if (Number.isInteger(h) & (h <= w)) {
+      if (brown === 2 * (h + w + 2)) {
+        answer.push(w + 2, h + 2);
+        return answer;
+      }
+    }
+  }
+
+  return answer;
+}
+
+// 참고하기: return Array.push('')를 실행하면 push 메서드의 반환값으로 새로운 배열의 길이를 반환한다!
