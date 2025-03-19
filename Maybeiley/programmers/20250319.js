df06c7d2a5a6680d9d462dd8f3df6203a6d903fe
Maybeiley <2784519@gmail.com>
2025-03-19 14:50:04 +0900

    자릿수 더하기 / 1단계
    https://school.programmers.co.kr/learn/courses/30/lessons/12931

--- Maybeiley/programmers/20250319.js
+++ Maybeiley/programmers/20250319.js
@@ -6,20 +6,36 @@ Leo가 본 카펫에서 갈색 격자의 수 brown, 노란색 격자의 수 yell
 - 제한사항 : 카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
 */
 
-function solution(brown, yellow) {
-  var answer = [];
+// function solution(brown, yellow) {
+//   var answer = [];
 
-  for (let w = 1; w <= yellow; w++) {
-    let h = yellow / w;
-    if (Number.isInteger(h) & (h <= w)) {
-      if (brown === 2 * (h + w + 2)) {
-        answer.push(w + 2, h + 2);
-        return answer;
-      }
-    }
-  }
+//   for (let w = 1; w <= yellow; w++) {
+//     let h = yellow / w;
+//     if (Number.isInteger(h) & (h <= w)) {
+//       if (brown === 2 * (h + w + 2)) {
+//         answer.push(w + 2, h + 2);
+//         return answer;
+//       }
+//     }
+//   }
+
+//   return answer;
+// }
+
+// 참고하기: return Array.push('')를 실행하면 push 메서드의 반환값으로 새로운 배열의 길이를 반환한다!
+
+/*
+[자릿수 더하기]
+자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+
+- 제한사항 : N의 범위는 100,000,000 이하의 자연수
+*/
+
+function solution(n) {
+  const answer = String(n)
+    .split('')
+    .reduce((a, b) => Number(a) + Number(b));
 
   return answer;
 }
-
-// 참고하기: return Array.push('')를 실행하면 push 메서드의 반환값으로 새로운 배열의 길이를 반환한다!
