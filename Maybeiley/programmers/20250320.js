1c5ff2158f7b8c2d892f478b9d608fd77619626f
Maybeiley <2784519@gmail.com>
2025-03-20 15:32:58 +0900

    숫자의 표현 / 2단계
    https://school.programmers.co.kr/learn/courses/30/lessons/12924

--- Maybeiley/programmers/20250320.js
+++ Maybeiley/programmers/20250320.js
@@ -3,12 +3,12 @@
 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
 */
 
-// function solution(strings, n) {
-//   const mix = strings.map((str) => str.slice(n, n + 1) + str);
-//   const answer = mix.sort().map((str) => str.slice(1));
+function solution(strings, n) {
+  const mix = strings.map((str) => str.slice(n, n + 1) + str);
+  const answer = mix.sort().map((str) => str.slice(1));
 
-//   return answer;
-// }
+  return answer;
+}
 
 // 추가 내용: sort는 비교 함수 결과가 음수, 0, 양수 값에 따라 정렬하기 때문에, 이것을 활용해서 n번째 글자를 비교하여 정렬할 수 있다.
 
@@ -42,3 +42,28 @@ function solution(a, b) {
 }
 
 // 추가 내용: 각각의 약수를 찾아 나누도록 풀이했는데, (a%i===0 && b%i===0) 조건으로 반복문을 써서 최대공약수로 한 번에 나누는 방법도 있다!
+
+/*
+[숫자의 표현]
+Finn은 요즘 수학공부에 빠져 있습니다. 수학 공부를 하던 Finn은 자연수 n을 연속한 자연수들로 표현 하는 방법이 여러개라는 사실을 알게 되었습니다. 예를들어 15는 다음과 같이 4가지로 표현 할 수 있습니다.
+
+1 + 2 + 3 + 4 + 5 = 15
+4 + 5 + 6 = 15
+7 + 8 = 15
+15 = 15
+자연수 n이 매개변수로 주어질 때, 연속된 자연수들로 n을 표현하는 방법의 수를 return하는 solution를 완성해주세요.
+*/
+
+function solution(n) {
+  var answer = 0;
+
+  for (let j = 1; j <= n; j++) {
+    if ((2 * n) % j === 0) {
+      let i = ((2 * n) / j - j + 1) / 2;
+      if (i > 0 && Number.isInteger(i)) answer++;
+    }
+  }
+  return answer;
+}
+
+// 추가 내용: 등차수열을 이용해서 n = i + (i+1) + (i+2) + ... + (i+(j-1)) = "j(2i+j-1) / 2"를 만족시키는 i, j 값을 찾는다
