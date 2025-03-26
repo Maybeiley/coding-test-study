d1993c48d15b14d7350d8902176d02298541331a
Seo-Yurim <yurim2204@naver.com>
2025-03-26 16:02:43 +0900

    날짜 비교하기 / 0단계 / 코드 개선

--- Seo-Yurim/2025-03-26/02.js
+++ Seo-Yurim/2025-03-26/02.js
@@ -5,8 +5,5 @@
 // 만약 date1이 date2보다 앞서는 날짜라면 1을, 아니면 0을 return 하는 solution 함수를 완성해 주세요.
 
 function solution(date1, date2) {
-  let firstDate = new Date(date1[0], date1[1], date1[2]);
-  let secondDate = new Date(date2[0], date2[1], date2[2]);
-
-  return firstDate < secondDate ? 1 : 0;
+  return new Date(date1) < new Date(date2) ? 1 : 0;
 }
