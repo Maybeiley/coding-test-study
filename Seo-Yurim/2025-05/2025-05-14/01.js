95fd433593b2f1d8913d237f5db3d5a34ea32f8e
Seo-Yurim <yurim2204@naver.com>
2025-05-14 20:56:35 +0900

    완주하지 못한 선수 / 1단계

---
+++ Seo-Yurim/2025-05/2025-05-14/01.js
@@ -0,0 +1,15 @@
+function solution(participant, completion) {
+  let count = {};
+
+  for (let name of participant) {
+    count[name] = (count[name] || 0) + 1;
+  }
+
+  for (let name of completion) {
+    count[name]--;
+  }
+
+  for (let name in count) {
+    if (count[name] > 0) return name;
+  }
+}
